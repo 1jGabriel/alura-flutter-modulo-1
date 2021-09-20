5546aa9d82c6a10efa6f1db56467a40c0687aafd
1jgabriel <gsantana152@gmail.com>
2021-09-20 16:02:46 -0300

    3rd class result code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Transferências',
-        ),
-      ),
-      body: const TransferList(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => print("abc"),
-      ),
-    ),
-  ));
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: TransferForm(),
+    ));
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+
+  final TextEditingController _amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Transfer Form")),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _accountNumberController,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                    labelText: 'Número da conta', hintText: '0000'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00'),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  final amount = double.tryParse(_amountController.text);
+                  final accountNumber = _accountNumberController.text;
+                  if (amount != null) {
+                    final transferPojo = TransferPojo(amount, accountNumber);
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text('$transferPojo')));
+                  }
+                },
+                child: const Text('Criar transferencia'))
+          ],
+        ));
+  }
 }
 
 class TransferList extends StatelessWidget {
@@ -24,11 +79,22 @@ class TransferList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(TransferPojo(2.0, '123')),
-        TransferItem(TransferPojo(3.0, '123')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Transferências',
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(TransferPojo(2.0, '123')),
+          TransferItem(TransferPojo(3.0, '123')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => debugPrint("abc"),
+      ),
     );
   }
 }
@@ -54,4 +120,9 @@ class TransferPojo {
   final String account;
 
   TransferPojo(this.value, this.account);
+
+  @override
+  String toString() {
+    return 'TransferPojo{value: $value, account: $account}';
+  }
 }
